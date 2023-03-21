c387825290dcc8dbe11e1d7c60147dc863f00441
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2023-03-21 16:11:15 +0000

    standard comments on project working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+    before_action :set_project
+
+    def create
+        @project.comments.create! params.required(:comment).permit(:content, :user_name)
+        redirect_to @project
+    end 
+
+    private
+
+    def set_project
+        @project = Project.find(params[:project_id])
+    end 
 end
